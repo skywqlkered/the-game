765f54cbc7e827408b3be19c271ed07b5587ce31
Julian <113358009+Julian-Dinnissen@users.noreply.github.com>
2024-10-27 18:50:15 +0100

    finished the game withoud axe implementation and reset the score

--- main_project.py
+++ main_project.py
@@ -1,123 +1,62 @@
 import random
-score = None
-game_state = None
-tree_state = None
+import io
+import sys
+from art import *
 
-wood = None
-axe = None
-crafting_table = None
+yes: list = ["yes", "yeah", "1", "ye", "y"]
+no: list = ["no", "nah", "0", "na", "n"]
+skip: str = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+score: int = 0
+game_state: bool = None
+tree_state: bool = None
 
-ground = [
-    "                   ",
-    "                   ",
-    "                   ",
-    "                   ",
-    "                   ",
-    "                   ",
-    "                   ",
-    "                   ",
-    "_______\\/__________",
-]
+wood: bool = False
+axe: bool = False
 
-tree = [
-    "                   ",
-    "        _____      ",
-    "     __|     |__   ",
-    "    |           |  ",
-    "    |           |  ",
-    "    |____   ____|  ",
-    "        |  |       ",
-    "        |  |       ",
-    "________|  |_______",
-]
+game_state: bool = None
 
-tree1 = [
-    "                   ",
-    "        _____      ",
-    "     __|     |__   ",
-    "    |           |  ",
-    "    |           |  ",
-    "    |____   ____|  ",
-    "        |  |       ",
-    "        \\  |       ",
-    "________|  |_______",
-]
 
-tree2 = [
-    "                   ",
-    "        ____       ",
-    "     __|    |__    ",
-    "    |          |   ",
-    "    |          |   ",
-    "    |___    ___|   ",
-    "        |__|       ",
-    "         __        ",
-    "________|  |_______",
-]
+def get_highscore() -> int:
+    scores = []
+    with open("score.txt", "r") as f:
+        for lines in f:
+            scores.append(lines.split(": ")[1])
+        if len(scores) == 0:
+            return 0
+    return int(max(scores))
 
-tree_fallen = [
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "                           ",
-    "               _____       ",
-    "          ____|     |__    ",
-    "  __      ____       __|   ",
-    "_|  |_________|_____|______",
-]
 
-steve = [
-    "                   ",
-    "                   ",
-    "                   ",
-    "        ___        ",
-    "       |   |       ",
-    "       |___|       ",
-    "      ___|___      ",
-    "     /   |   \\     ",
-    "________/ \\________",
-]
+def save_scorefile(name: str, score: int) -> None:
+    file_lines = []
+    with open("score.txt", "r") as f:
+        for lines in f:
+            file_lines.append(lines)
+    if get_highscore() < score:
+        with open("score.txt", "w") as f:
+            f.write(f"{name}: {score}\n")
 
-steve_fallen = [
-    "                         ",
-    "                         ",
-    "                         ",
-    "            ____   \\/    ",
-    "         __|    |___|__  ",
-    "        |__      ______  ",
-    "           |____|/ _|_ \\ ",
-    "                  |x x|  ",
-    "__________________|___|__",
-]
+            if len(file_lines) > 10:
+                file_lines.pop(10)
+            for i in range(len(file_lines)):
+                f.write(file_lines[i])
 
-empty_slot = [
-    " ______ ",
-    "|      | ",
-    "|      | ",
-    "|______| "
-]
+    if get_highscore() > score and len(file_lines) < 11:
+        with open("score.txt", "w") as f:
+            f.write(f"{name}: {score}\n")
 
-axe_slot = [
-    " ______  ",
-    "|  |D) | ",
-    "|  |   | ",
-    "|__|___| "
-]
 
-wood_slot = [
-    " ______ ",
-    "|  __  | ",
-    "| |__| | ",
-    "|______| "
-]
-game_state = None
+def read_scorefile() -> None:
+    print(skip)
+    print("SCOREBOARD:")
+    with open("score.txt", "r") as f:
+        for lines in f:
+            print(f"    {lines.replace('\n', '')}")
+    print("")
 
 
 def start_game() -> bool:
-    global game_state
-    yes: list = ["yes", "yeah", "1", "ye", "y"]
-    no: list = ["no", "nah", "0", "na", "n"]
+    global game_state, score
+    score = 0
 
     answer = input("Do you want to play a game? ")
     while answer.lower() not in yes + no:
@@ -126,22 +65,44 @@ def start_game() -> bool:
         game_state = True
     elif answer.lower() in no:
         game_state = False
+        answer = input("Do you want to see the scoreboard? ")
+        if answer.lower() in yes:
+            read_scorefile()
+        elif answer.lower() in no:
+            exit()
+
 
 def show_inventory() -> None:
-    pass
+    print(skip)
+    inventory = []
+    if wood and not axe:
+        for i in range(len(empty_slot)):
+            inventory.append(wood_slot[i] + empty_slot[i] + empty_slot[i])
+    if wood and axe:
+        for i in range(len(empty_slot)):
+            inventory.append(wood_slot[i] + axe_slot[i] + empty_slot[i])
 
-def update_score() -> int:
+    if not wood and not axe:
+        for i in range(len(empty_slot)):
+            inventory.append(empty_slot[i] + empty_slot[i] + empty_slot[i])
+    for i in range(len(inventory)):
+        print(inventory[i])
+    input("\nPress enter to continue...")
+
+
+def update_score() -> None:
     global score
-    score =+ 1
-    
+    score += 1
 
-def show_score()-> int:
+
+def show_current_score() -> int:
     global score
     return score
 
+
 def draw_game() -> None:
     global tree_state
-    print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
+    print(skip)
     tree_state = None
     board = []
     if random.randint(0, 2) == 0:
@@ -152,52 +113,135 @@ def draw_game() -> None:
     else:
         tree_state = False
         for i in range(len(ground)):
-            board.append(steve[i] + ground[i]+ ground[i])
+            board.append(steve[i] + ground[i] + ground[i])
 
     for i in range(len(board)):
         print(board[i])
 
 
+def death() -> None:
+    global game_state
+    game_state = False
+
+    print(skip)
+    if get_highscore() < show_current_score():
+        name = input(
+            """\nYOU GOT A HIGH SCORE!
+Enter your name:
+        """
+        )
+        save_scorefile(name.upper(), show_current_score())
+
+    elif get_highscore() > show_current_score():
+        print(
+            f"""YOU DIED!
+            Your score was: {show_current_score()} 
+            """
+        )
+    start_game()
+
+
+def show_options() -> list:
+    options_list = ["1. Go right", ". Cut down tree", ". Show inventory"]
+    available_options = [options_list[0]]
+    counter = 2
+    if tree_state:
+        available_options.append(f"{counter}{options_list[1]}")
+        counter += 1
+
+    if wood:
+        available_options.append(f"{counter}{options_list[2]}")
+        counter += 1
+
+    return available_options
+
+
 start_game()
-print(game_state)
+
+
+def option_r() -> None:
+    if random.randint(0, 4) == 0:
+        print(skip)
+        print("\nYou ran trough the field")
+        print("The snake got you")
+        input("\nPress enter to continue...")
+        print(skip)
+        for i in range(len(snake)):
+            print(snake[i])
+        input("\nPress enter to continue...")
+        death()
+    else:
+        update_score()
+
+
+def option_t() -> None:
+    if random.randint(0, 4) == 0:
+        print(skip)
+        print("\nYou cut down the tree!")
+        print("The tree fell on you")
+        input("\nPress enter to continue...")
+        print(skip)
+        for i in range(len(steve_fallen)):
+            print(steve_fallen[i])
+        input("\nPress enter to continue...")
+        death()
+    else:
+        input(skip + "You now have wood! Press enter to continue...")
+        wood = True
+        update_score()
+
+
+def option_i() -> None:
+    show_inventory()
+
+
 while game_state:
     draw_game()
-    print("\n")
-    if not tree_state:
-        option = int(
-            input(
-                """What do you want to do?
-        1. Go right\n
-        """
-            )
-        )
+    print(score)
+    print("\nWhat do you want to do?")
+    for i in show_options():
+        print(f"    {i}")
+    option = input("\n    ")
 
-        if option == 1:
-            update_score()
-            draw_game()
+    try:
+        int(option)
+    except ValueError:
+        print("\nWhat do you want to do?")
+        for i in show_options():
+            print(f"    {i}")
+        option = input("\n    ")
 
-    if tree_state:
-        option = int(
-            input(
-                """What do you want to do?
-        1. Go right
-        2. Cut down tree\n
-        """
-            )
-        )
+    if len(show_options()) == 1:
+        while option not in ["1"]:
+            print("\nWhat do you want to do?")
+            for i in show_options():
+                print(f"    {i}")
+            option = input("\n    ")
 
-        if option == 1:
-            update_score()
-            draw_game()
-        elif option == 2:
-            if random.randint(0, 4) == 0:
-                print(f"\nYour score is {show_score()}\n")
+    if len(show_options()) == 2:
+        while option not in ["1", "2"]:
+            print("\nWhat do you want to do?")
+            for i in show_options():
+                print(f"    {i}")
+            option = input("\n    ")
 
-                raise Exception("You died!")
-            else:
-                input("You now have wood! Press enter to continue...")
-                wood = True
-                update_score()
-                draw_game()
-    
+    if len(show_options()) == 3:
+        while option not in ["1", "2", "3"]:
+            print("\nWhat do you want to do?")
+            for i in show_options():
+                print(f"    {i}")
+            option = input("\n    ")
+
+    if int(option) == 1:
+        option_r()
+
+    elif int(option) == 2 and not tree_state and wood and len(show_options()) == 2:
+        option_i()
+
+    elif int(option) == 2 and tree_state and len(show_options()) == 2:
+        option_t()
+
+    elif int(option) == 3 and tree_state and wood and len(show_options()) == 3:
+        option_i()
+
     
